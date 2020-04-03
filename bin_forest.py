bbbd1c435db9006c38b759a230fdbb62dbdcabc0
VitalyGladyshev <glvv232@mail.ru>
2020-04-03 18:00:49 +0300

    ДЗ 5

---
+++ bin_forest.py
@@ -0,0 +1,254 @@
+import numpy as np
+import random
+
+__all__ = ["Node","Leaf","get_bootstrap","get_subsample",
+            "gini","quality","split","find_best_split","build_tree","random_forest",
+            "classify_object","predict","tree_vote","print_tree","accuracy_metric","get_meshgrid"]
+
+
+class Node:
+    # Класс узла
+    
+    def __init__(self, index, t, true_branch, false_branch):
+        self.index = index  # индекс признака, по которому ведется сравнение с порогом в этом узле
+        self.t = t  # значение порога
+        self.true_branch = true_branch  # поддерево, удовлетворяющее условию в узле
+        self.false_branch = false_branch  # поддерево, не удовлетворяющее условию в узле
+
+
+class Leaf:
+    # Класс терминального узла (листа)
+    
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
+        self.prediction = self.predict()
+        
+    def predict(self):
+        # подсчет количества объектов разных классов
+        classes = {}  # сформируем словарь "класс: количество объектов"
+        for label in self.labels:
+            if label not in classes:
+                classes[label] = 0
+            classes[label] += 1
+        #  найдем класс, количество объектов которого будет максимальным в этом листе и вернем его    
+        prediction = max(classes, key=classes.get)
+        return prediction
+
+
+def get_bootstrap(data, labels, N):
+    random.seed(42)
+    n_samples = data.shape[0]
+    bootstrap = []
+
+    for i in range(N):
+        b_data = np.zeros(data.shape)
+        b_labels = np.zeros(labels.shape)
+        
+        for j in range(n_samples):
+            sample_index = random.randint(0, n_samples-1)
+            b_data[j] = data[sample_index]
+            b_labels[j] = labels[sample_index]
+        bootstrap.append((b_data, b_labels))
+        
+    return bootstrap
+
+
+def get_subsample(len_sample):
+    # будем сохранять не сами признаки, а их индексы
+    sample_indexes = [i for i in range(len_sample)]
+    
+    len_subsample = int(np.sqrt(len_sample))
+    subsample = []
+    
+    random.shuffle(sample_indexes)
+    for _ in range(len_subsample):
+        subsample.append(sample_indexes.pop())
+        
+    return subsample
+
+
+def gini(labels):
+    # Расчет критерия Джини
+
+    #  подсчет количества объектов разных классов
+    classes = {}
+    for label in labels:
+        if label not in classes:
+            classes[label] = 0
+        classes[label] += 1
+    
+    #  расчет критерия
+    impurity = 1
+    for label in classes:
+        p = classes[label] / len(labels)
+        impurity -= p ** 2
+        
+    return impurity
+
+
+def quality(left_labels, right_labels, current_gini):
+    # Расчет качества
+
+    # доля выбоки, ушедшая в левое поддерево
+    p = float(left_labels.shape[0]) / (left_labels.shape[0] + right_labels.shape[0])
+    
+    return current_gini - p * gini(left_labels) - (1 - p) * gini(right_labels)
+
+
+def split(data, labels, index, t):
+    # Разбиение датасета в узле
+    
+    left = np.where(data[:, index] <= t)
+    right = np.where(data[:, index] > t)
+        
+    true_data = data[left]
+    false_data = data[right]
+    true_labels = labels[left]
+    false_labels = labels[right]
+        
+    return true_data, false_data, true_labels, false_labels
+
+
+def find_best_split(data, labels):
+    # Нахождение наилучшего разбиения
+
+    #  обозначим минимальное количество объектов в узле
+    min_leaf = 1
+
+    current_gini = gini(labels)
+
+    best_quality = 0
+    best_t = None
+    best_index = None
+    
+    n_features = data.shape[1]
+    
+    # выбор индекса из подвыборки длиной sqrt(n_features)
+    subsample = get_subsample(n_features)
+    
+    for index in subsample:
+        # будем проверять только уникальные значения признака, исключая повторения
+        t_values = np.unique([row[index] for row in data])
+        
+        for t in t_values:
+            true_data, false_data, true_labels, false_labels = split(data, labels, index, t)
+            #  пропускаем разбиения, в которых в узле остается менее 5 объектов
+            if len(true_data) < min_leaf or len(false_data) < min_leaf:
+                continue
+            
+            current_quality = quality(true_labels, false_labels, current_gini)
+            
+            #  выбираем порог, на котором получается максимальный прирост качества
+            if current_quality > best_quality:
+                best_quality, best_t, best_index = current_quality, t, index
+
+    return best_quality, best_t, best_index
+
+
+def build_tree(data, labels):
+    # Построение дерева с помощью рекурсивной функции
+
+    quality, t, index = find_best_split(data, labels)
+
+    #  Базовый случай - прекращаем рекурсию, когда нет прироста в качества
+    if quality == 0:
+        return Leaf(data, labels)
+
+    true_data, false_data, true_labels, false_labels = split(data, labels, index, t)
+
+    # Рекурсивно строим два поддерева
+    true_branch = build_tree(true_data, true_labels)
+    false_branch = build_tree(false_data, false_labels)
+
+    # Возвращаем класс узла со всеми поддеревьями, то есть целого дерева
+    return Node(index, t, true_branch, false_branch)
+
+
+def random_forest(data, labels, n_trees):
+    forest = []
+    bootstrap = get_bootstrap(data, labels, n_trees)
+    
+    for b_data, b_labels in bootstrap:
+        forest.append(build_tree(b_data, b_labels))
+        
+    return forest
+
+
+def classify_object(obj, node):
+
+    #  Останавливаем рекурсию, если достигли листа
+    if isinstance(node, Leaf):
+        answer = node.prediction
+        return answer
+
+    if obj[node.index] <= node.t:
+        return classify_object(obj, node.true_branch)
+    else:
+        return classify_object(obj, node.false_branch)
+
+
+def predict(data, tree):
+    
+    classes = []
+    for obj in data:
+        prediction = classify_object(obj, tree)
+        classes.append(prediction)
+    return classes
+
+
+def tree_vote(forest, data):
+    # предсказание голосованием деревьев
+
+    # добавим предсказания всех деревьев в список
+    predictions = []
+    for tree in forest:
+        predictions.append(predict(data, tree))
+    
+    # сформируем список с предсказаниями для каждого объекта
+    predictions_per_object = list(zip(*predictions))
+    
+    # выберем в качестве итогового предсказания для каждого объекта то,
+    # за которое проголосовало большинство деревьев
+    voted_predictions = []
+    for obj in predictions_per_object:
+        voted_predictions.append(max(set(obj), key=obj.count))
+        
+    return voted_predictions
+
+
+def print_tree(node, spacing=""):
+    # Напечатаем ход нашего дерева
+
+    # Если лист, то выводим его прогноз
+    if isinstance(node, Leaf):
+        print(spacing + "Прогноз:", node.prediction)
+        return
+
+    # Выведем значение индекса и порога на этом узле
+    print(spacing + 'Индекс', str(node.index))
+    print(spacing + 'Порог', str(node.t))
+
+    # Рекурсионный вызов функции на положительном поддереве
+    print (spacing + '--> True:')
+    print_tree(node.true_branch, spacing + "  ")
+
+    # Рекурсионный вызов функции на положительном поддереве
+    print (spacing + '--> False:')
+    print_tree(node.false_branch, spacing + "  ")
+
+
+def accuracy_metric(actual, predicted):
+    # Введем функцию подсчета точности как доли правильных ответов
+
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+def get_meshgrid(data, step=.05, border=1.2):
+    x_min, x_max = data[:, 0].min() - border, data[:, 0].max() + border
+    y_min, y_max = data[:, 1].min() - border, data[:, 1].max() + border
+    return np.meshgrid(np.arange(x_min, x_max, step), np.arange(y_min, y_max, step))
